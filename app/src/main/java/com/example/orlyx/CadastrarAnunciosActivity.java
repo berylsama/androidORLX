190f0db9147c785f8aa317bc0f5226b4bfb1771d
THABATA GUERREIRO NASCIMENTO DE MORAES <thabata.gnmoraes@senacsp.edu.br>
2020-03-11 20:39:48 -0300

    Validação da permissão

--- app/src/main/java/com/example/orlyx/CadastrarAnunciosActivity.java
+++ app/src/main/java/com/example/orlyx/CadastrarAnunciosActivity.java
@@ -1,8 +1,10 @@
 package com.example.orlyx;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -15,6 +17,7 @@ import com.santalu.maskedittext.MaskEditText;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class CadastrarAnunciosActivity extends AppCompatActivity
 
@@ -27,7 +30,8 @@ public class CadastrarAnunciosActivity extends AppCompatActivity
         private Spinner campoEstado, campoCategoria;
         private Anuncio anuncio;
         private StorageReference storage;
-        
+        private android.app.AlertDialog dialog;
+
         private String [] permissoes = new String[]{
                 Manifest.permission.READ_EXTERNAL_STORAGE,
         };
@@ -50,13 +54,16 @@ public class CadastrarAnunciosActivity extends AppCompatActivity
     }
     
     public void salvarAnuncio(View v){
-        
+
+        dialog = new SpotsDialog.Builder().setContext(this).setMessage("Salvando Anúncio...").setCancelable("false").build();
+
         for (int i = 0; i <listaFotosRecuperadas.size(); i++){
             String urlimagem = listaFotosRecuperadas.get(i);
             int tamanhoLista = listaFotosRecuperadas.size();
             salvarFotosStorage (urlimagem,tamanhoLista,i);
         }
     }
+
 
     private void salvarFotosStorage(String urlimagem, int tamanhoLista, int i) {
     }
@@ -65,7 +72,23 @@ public class CadastrarAnunciosActivity extends AppCompatActivity
     }
 
     private void inicializarComponentes() {
-        
+
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDesc = findViewById(R.id.editDesc);
+        campoValor = findViewById(R.id.editValor);
+        campoContato = findViewById(R.id.editContato);
+        imagem1 = findViewById(R.id.imgCadastro1);
+        imagem2 = findViewById(R.id.imgCadastro2);
+        imagem3 = findViewById(R.id.imgCadastro3);
+        campoEstado = findViewById(R.id.spinnerEstado);
+        campoCategoria = findViewById(R.id.spinnerCategoria);
+
+        imagem1.setOnClickListener(this);
+        imagem2.setOnClickListener(this);
+        imagem3.setOnClickListener(this);
+
+        Locale locale = new Locale("pt","BR");
+        campoValor.setLocale(locale);
     }
 
     @Override
@@ -73,6 +96,24 @@ public class CadastrarAnunciosActivity extends AppCompatActivity
 
     }
 
+    public void validarDadosAnuncio (View view){
+
+    }
+
+    public void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas :(");
+        builder.setMessage("Para utilizar o app, é necessário aceitar as permissões!");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
   }
 
 
--- app/src/main/java/com/example/orlyx/Permissoes.java
+++ app/src/main/java/com/example/orlyx/Permissoes.java
@@ -1,16 +1,17 @@
 package com.example.orlyx;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class Permissoes {
-    public static boolean validarPermissoes(String[] permissoes, CadastrarAnunciosActivity cadastrarAnunciosActivity, int requestCode) {
+    public static boolean validarPermissoes(String[] permissoes, Activity activity, int requestCode) {
      if(Build.VERSION.SDK_INT>=23){
          List<String>listaPermissoes = new ArrayList<>();
 
@@ -19,6 +20,12 @@ class Permissoes {
              Boolean temPermissoes = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
              if (!temPermissoes) listaPermissoes.add(permissao);
          }
+            if (listaPermissoes.isEmpty())return true;
+            String[] novasPermissoes =new String[listaPermissoes.size()];
+            listaPermissoes.toArray(novasPermissoes);
+
+         ActivityCompat.requestPermissions(activity,novasPermissoes,requestCode);
      }
+     return true;
     }
 }
